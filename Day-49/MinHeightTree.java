d7c72eca335b50aaa09b48f9c118c3a832bd8425
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-05-18 23:18:07 +0530

    Updated min Height tree using dfs approach

--- Day-49/MinHeightTree.java
+++ Day-49/MinHeightTree.java
@@ -1,3 +1,4 @@
+
 /*
 A tree is an undirected graph in which any two vertices are connected by exactly one path. 
 In other words, any connected graph without simple cycles is a tree.
@@ -53,25 +54,94 @@ Constraints:
 -> The given input is guaranteed to be a tree and there will be no repeated edges.
  */
 import java.util.*;
+class Graph {
+    private final int n;
+    private final List<List<Integer>> adjLst;
 
-public class MinHeightTree {
-    public static List<Integer> check(int n, int edges[][]) {
-        //Write your code here and return the list
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int edges[][] = new int[m][2];
-        for (int i = 0; i < m; i++) {
-            int a = sc.nextInt(), b = sc.nextInt();
-            edges[i][0] = a;
-            edges[i][1] = b;
+    private void buildAdjLst(int[][] edges) {
+
+        for (int i = 0; i < n; i++) {
+            adjLst.add(new ArrayList<>());
         }
-        sc.close();
-        System.out.println(check(n, edges));
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adjLst.get(u).add(v);
+            adjLst.get(v).add(u);
+        }
     }
 
-    
+    public Graph(int n, int[][] edges) {
+        this.n = n;
+        this.adjLst = new ArrayList<>();
+        buildAdjLst(edges);
+    }
+
+    private int dfs(int node, boolean[] visited) {
+        visited[node] = true;
+        if (adjLst.get(node).isEmpty()) { // Base case is when i reached the leaf node 
+            return 0;
+        }
+
+        int maxi = 0;
+        for (int nbr : adjLst.get(node)) {
+            if (!visited[nbr]) {
+                maxi = Math.max(maxi, 1 + dfs(nbr, visited));
+            }
+        }
+
+        return maxi;
+    }
+
+    public List<Integer> getMHTsLabels() {
+        List<Integer> ans = new ArrayList<>();
+
+        // do dfs from each node
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]); // (ind,height)
+        for (int i = 0; i < n; i++) {
+            int height = dfs(i, new boolean[n]);
+            pq.offer(new int[] { i, height });
+        }
+
+        int[] mht = pq.poll();
+        ans.add(mht[0]);
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll(); // (ind,height)
+
+            if (curr[1] == mht[1]) {
+                ans.add(curr[0]);
+            } else {
+                break;
+            }
+        }
+
+        return ans;
+
+    }
 }
 
+public class MinHeightTree {
+    private List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        Graph g = new Graph(n,edges);
+
+        return g.getMHTsLabels();
+    }
+
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int edges[][] = new int[m][2];
+            for (int i = 0; i < m; i++) {
+                int a = sc.nextInt(), b = sc.nextInt();
+                edges[i][0] = a;
+                edges[i][1] = b;
+            }
+            System.out.println(new MinHeightTree().findMinHeightTrees(n, edges));
+        }
+    }
+
+}
