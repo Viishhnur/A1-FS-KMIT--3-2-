baa217d33d96ce12389d0c344ce65f9feee5fdbc
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-05-26 16:54:33 +0530

    Updated Day-1 basic spring boot operations

---
+++ Day-55 Spring Boot Day-1/employeeManagementBackend/src/test/java/com/codecraftv/employeeManagementBackend/EmployeeManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecraftv.employeeManagementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
