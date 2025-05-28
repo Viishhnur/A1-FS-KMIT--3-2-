24980b410624e6773858166dac11ff89bd154812
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-05-28 16:43:24 +0530

    Updated singleton design pattern examples

---
+++ Day-60/Day60_InClass_Codes/Singleton.java
@@ -0,0 +1,35 @@
+// In this code snippet we are creating the object through static initilizer block only once
+// But this is not suitable , we should use lazy instatializtion
+class Singleton
+{
+	private int value;
+	private static final Singleton obj;
+
+	private Singleton() {	}
+
+	public synchronized int getValue() {
+		return value;
+	}
+
+	public synchronized void setValue(int value) {
+		this.value = value;
+	}
+
+	static { obj = new Singleton(); }
+	{ value = 10; }
+
+	public static Singleton getInstance() {
+		return obj;
+	}
+
+	public static void main(String []args) 
+	{
+		Singleton s1 = Singleton.getInstance();  
+		System.out.println("s1 value is " + s1.getValue());	
+		s1.setValue(30);
+
+		Singleton s2 = Singleton.getInstance();	
+		System.out.println("s1 value is " + s1.getValue());	
+		System.out.println("s2 value is " + s2.getValue());	
+  }
+}
---
+++ Day-60/Day60_InClass_Codes/Singleton3.java
@@ -0,0 +1,83 @@
+// Here i am using multiple threads 
+class Singleton
+{
+	private int value;
+
+	/* volatile variables are used in Concurrent programming in Java. When we declare
+	a variable volatile, every thread reads its value from main memory and doesn't 
+	used cached value available in every thread stack. */
+
+	private static volatile Singleton obj;
+
+	private Singleton() {
+		value = 10;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public static Singleton getInstance() {
+		if(obj == null) {
+		  System.out.println("if block");	
+		  obj = new Singleton();
+		}
+		else {
+		  System.out.println("else block");	
+		}
+		return obj;
+	}
+
+	public static Singleton getInstance2() {
+		if(obj == null) {
+		  synchronized(Singleton.class) {
+			if(obj == null) {
+			  System.out.println("if block");	
+			  obj = new Singleton();
+			}
+		  }
+		}
+		else {
+		  System.out.println("else block");	
+		}
+		return obj;
+	}
+    public void destructor(){
+        obj = null;
+    }
+	public static void main(String []args) 
+	{
+		Singleton s1 = Singleton.getInstance2();    
+		System.out.println("s1 value is " + s1.getValue());	
+		s1.setValue(30);
+
+		Singleton s2 = Singleton.getInstance2();	
+		System.out.println("s1 value is " + s1.getValue());	
+		System.out.println("s2 value is " + s2.getValue());	
+
+        s1.destructor();
+        System.out.println("After destructor " + s1.getValue());
+        System.out.println(s1 == s2);
+
+        // Try creating threads and access
+        // Thread t1 = new Thread(()->{
+        //     Singleton.getInstance2();
+        // });
+
+        // Thread t2 = new Thread(()->{
+        //     Singleton.getInstance2();
+        // });
+
+        // t1.start();
+        // t2.start();
+
+        // System.out.println("Main ends");
+	}
+}
+
+// http://www.ibm.com/developerworks/ibm/library/it-haggar_bytecode/#4
+// javap -c Singleton > singleton2.bc
