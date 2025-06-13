0027373eedd6053d0f6b99484511765ed71c698b
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-06-14 01:47:58 +0530

    Updated with recursion + memoization + tabulation

--- Day-69 DP-5/Jump_Game.java
+++ Day-69 DP-5/Jump_Game.java
@@ -1,4 +1,5 @@
-/*
+
+/* Jump Game-ii) Leetcode - 45
 You are participating in a futuristic space exploration mission where you must navigate through a series of planets aligned in a straight line.
 The planets are numbered from 0 to n-1, and you start your journey on planet 0.
 
@@ -25,6 +26,108 @@ Explanation:
 - Move from planet 0 to planet 1.
 - Move from planet 1 to planet 4 (last planet).
  */
-public class Jump_Game {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Jump_Game {
+    private int solveRec(int idx, int jumps, int[] nums, final int n) {
+        if (idx >= n - 1) {
+            return jumps;
+        }
+
+        int maxJump = nums[idx];
+        int minJumps = Integer.MAX_VALUE;
+        for (int i = idx; i < idx + maxJump && i < n; i++) {
+            minJumps = Math.min(minJumps, solveRec(i + 1, jumps + 1, nums, n));
+        }
+
+        return minJumps;
+    }
+
+    private int solveRec2(int idx, int[] nums, final int n) {
+        if (idx >= n - 1) {
+            return 0;
+        }
+
+        int maxJump = nums[idx];
+        int minJumps = Integer.MAX_VALUE;
+        for (int i = idx; i < idx + maxJump && i < n; i++) {
+            int steps = solveRec2(i + 1, nums, n);
+            if (steps != Integer.MAX_VALUE) {
+                minJumps = Math.min(minJumps, 1 + steps);
+            }
+        }
+
+        return minJumps;
+    }
+
+    private int solveMemo(int idx, int[] nums, final int n, int[] dp) {
+        if (idx >= n - 1) {
+            return 0;
+        }
+
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+        int maxJump = nums[idx];
+        int minJumps = Integer.MAX_VALUE;
+        for (int i = idx; i < idx + maxJump && i < n; i++) {
+            int steps = solveMemo(i + 1, nums, n, dp);
+            if (steps != Integer.MAX_VALUE) {
+
+                minJumps = Math.min(minJumps, 1 + steps);
+            }
+        }
+
+        return dp[idx] = minJumps;
+    }
+
+    private int solveTab(int[] nums, final int n) {
+        int[] dp = new int[n];
+
+        dp[n - 1] = 0; // base case
+
+        for (int idx = n - 2; idx >= 0; idx--) {
+            int maxJump = nums[idx];
+            int minJumps = Integer.MAX_VALUE;
+            for (int i = idx; i < idx + maxJump && i + 1 < n; i++) {
+                int steps = dp[i + 1];
+                if (steps != Integer.MAX_VALUE) {
+
+                    minJumps = Math.min(minJumps, 1 + steps);
+                }
+            }
+
+            dp[idx] = minJumps;
+        }
+
+        return dp[0];
+    }
+
+    public int minJumps(int[] nums) {
+        int n = nums.length;
+
+        // Approach-i) Recursion(TLE)
+        // return solveRec(0,0,nums,n);
+
+        // Approach-ii) Recursion another variant do not pass jumps as argument
+        // return solveRec2(0,nums,n);
+
+        // Approach-iii) Memoization
+        // int[] dp = new int[n];
+        // Arrays.fill(dp,-1);
+        // return solveMemo(0,nums,n,dp);
+
+        // Approach-iv) Tabulation
+        return solveTab(nums, n);
+    }
+
+    public static void main(String[] args) {
+        int[] nums;
+        try (Scanner sc = new Scanner(System.in)) {
+            nums = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        System.out.println(new Jump_Game().minJumps(nums));
+    }
 }
